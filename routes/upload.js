d501e6c0180fd5b83bdd4782a809a8b93bc71a75
aravind <arvindsuresh2009@gmail.com>
2015-03-20 02:43:50 +0530

    done

--- routes/upload.js
+++ routes/upload.js
@@ -1,24 +1,31 @@
 /**
  * Created by vignesh on 11/2/15.
  */
-var express = require('express');
-var router = express.Router();
-var mysql=require('mysql');
-var db = require('../public/db_structure');
-var notification = require('./util/notification');
-var isAuth = function(req, res, next) {
+ var express = require('express');
+ var router = express.Router();
+ var mysql=require('mysql');
+ var db = require('../public/db_structure');
+ var notification = require('./util/notification');
+ var isAuth = function(req, res, next) {
     console.log('Authenticating');
     next();
-    /*if (req.isAuthenticated())
+    if (req.isAuthenticated())
         next();
     else {
         req.flash('login', 'LOGIN');
         res.redirect('/login')
-    }*/
+    }
 };
-router.get('/',isAuth,function(req,res){
+router.get('/',isAuth,function(req, res){
     res.render('upload');
 });
+/*router.get('/get/all', isAuth, function(req, res) {
+    var querystring = "SELECT * FROM noteshare.uploads WHERE userid=" + mysql.escape(req.user.id);
+    db.querydb(querystring,function(result){
+        console.log(querystring);
+        res.end(JSON.stringify(result));
+    });
+});*/
 router.post('/',isAuth,function(req,res){
     console.log(req.files);
     var files = req.files.upload;
@@ -26,9 +33,9 @@ router.post('/',isAuth,function(req,res){
         res.end('error no files sent');
     } else {
         var querystring="INSERT INTO noteshare.uploads(userid,name,filename,views,rating) VALUES(";
-        if(req.user && req.user.id){
-            querystring+=mysql.escape(req.user.id)+',';
-        } else {
+            if(req.user && req.user.id){
+                querystring+=mysql.escape(req.user.id)+',';
+            } else {
             //TODO should throw error
             querystring+='1,';
         }
@@ -39,12 +46,12 @@ router.post('/',isAuth,function(req,res){
         }
         querystring+=mysql.escape(files.name)+',';
         querystring+='0,3);';
-        db.querydb(querystring,function(result){
-            console.log(querystring);
-            notification.notifyAllFollowers(req.user.id,"upload",req.user.username + " has uploaded a file : " + files.originalname);
-            res.end(JSON.stringify(result));
-        })
-    }
+db.querydb(querystring,function(result){
+    console.log(querystring);
+    notification.notifyAllFollowers(req.user.id,"upload",req.user.username + " has uploaded a file : " + files.originalname);
+    res.end(JSON.stringify(result));
+})
+}
 });
 
 
