48d79c53db0a33064aee8939c282fb55f08f0bb6
aravind <arvindsuresh2009@gmail.com>
2015-03-20 12:02:18 +0530

    done

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));git
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'SeKRetKeeY' } ));
 require('./routes/passport')(passport);
 app.use(passport.initialize());
